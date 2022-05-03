ea394e0432c9f981b68e34b55986199f7daf5ea5
ItsTrashPanda <72630588+ItsTrashPanda@users.noreply.github.com>
2022-05-03 16:59:03 -0700

    fixed issues with class c didn't fix same issues with class a and b bcuz im lazy, and I added a temporary script to main that interfaces with the SubnetCalculator class for testing via console

--- src/main/java/ITBox/application/Main.java
+++ src/main/java/ITBox/application/Main.java
@@ -1,10 +1,43 @@
 package ITBox.application;
-import ITBox.tools.*;
+
+import ITBox.tools.SubnetCalculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        String sClass = "";
+        String input = "";
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        SubnetCalculator calc;
 
-
-        thanks
+        System.out.println("Please enter a Subnet Class");
+        sClass = scanner.nextLine();
+        System.out.println("Please enter a host size or an x");
+        while (!input.equals("x")) {
+            input = scanner.nextLine();
+            if (!input.equals("x")) {
+                list.add(input);
+            }
+        }
+        for(String s:list) {
+            intList.add(Integer.parseInt(s));
+        }
+        calc = new SubnetCalculator(sClass, intList);
+        calc.calculate();
+        int j = 0;
+        for(String[] i: calc.getIpRanges()) {
+            j++;
+            System.out.println("Subnet " + j);
+            System.out.println("Network Address: " + i[0]);
+            System.out.println("First Host Address: " + i[1]);
+            System.out.println("Last Host Address: " + i[2]);
+            System.out.println("Broadcast Address: " + i[3]);
+            System.out.println("Subnet Mask: " + i[4]);
+        }
     }
 }
--- src/main/java/ITBox/tools/SubnetCalculator.java
+++ src/main/java/ITBox/tools/SubnetCalculator.java
@@ -3,9 +3,9 @@ package ITBox.tools;
 import java.util.ArrayList;
 
 public class SubnetCalculator {
-    private char sClass;
+    private String sClass;
     private ArrayList<Integer> sHosts;
-    private String[][] ipRanges;
+    private String[][] ipRanges = new String[4][5];
 
     private boolean classful = false;
 
@@ -16,11 +16,10 @@ public class SubnetCalculator {
      * Calculates Subnets.
      * <p>
      * Used to find the IP address ranges for a customizable subnet
-     *
-     * @param  initClass The Network Class of choice in the form of a char, Must be CAPITALIZED.
+     *  @param  initClass The Network Class of choice in the form of a char, Must be CAPITALIZED.
      * @param  initHosts An ArrayList filled with the required number of Hosts in each subnet.
      */
-    public SubnetCalculator(char initClass, ArrayList<Integer> initHosts) {
+    public SubnetCalculator(String initClass, ArrayList<Integer> initHosts) {
         sClass = initClass;
         sHosts = initHosts;
     }
@@ -30,27 +29,25 @@ public class SubnetCalculator {
         int s2Seg;
         int s3Seg = 0;
         int hostSeg = 0;
-        String networkAddress;
-        String firstHostAddress;
-        String lastHostAddress;
-        String broadcastAddress;
+        String networkAddress = "";
+        String firstHostAddress = "";
+        String lastHostAddress = "";
+        String broadcastAddress = "";
         String subnetMask;
         switch (sClass) {
-            case 'A':
-                break;
-            case 'B':
+            case "A": {
                 //Setting the first twos segments as they will either never change or start at a non-0 number
-                s1Seg = 172;
-                s2Seg = 16;
+                s1Seg = 10;
+                s2Seg = 0;
                 //"all that jazz" -adam
-                for(int i:sHosts) {
+                for (int i : sHosts) {
                     //Returns error value of -1 (Subnet Size too large)
-                    if(i+2 > 65536) {
-                        return(-1);
+                    if (i + 2 > 16777216) {
+                        return (-1);
                     }
                     //Rounding up to the nearest square of two
                     int j = 0;
-                    while(i+2 <= nearest[j]) {
+                    while (i + 2 <= nearest[j]) {
                         j++;
                     }
                     //Converting the int segments into a whole dot separated address
@@ -58,13 +55,13 @@ public class SubnetCalculator {
                     hostSeg += 1;
                     firstHostAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
                     hostSeg += nearest[j] - 2;
-                    if(hostSeg >= 255) {
-                        s3Seg = (int)(hostSeg+1/256);
-                        hostSeg = ((hostSeg+1/256)-1-s3Seg)*256;
-                        if(s3Seg >= 255) {
-                            s2Seg = (int)(s3Seg+1/256);
-                            s3Seg = ((s3Seg+1/256)-1-s2Seg)*256;
-                            if(s2Seg >= 32) {
+                    if (hostSeg >= 255) {
+                        s3Seg = (int) (hostSeg + 1 / 256);
+                        hostSeg = ((hostSeg + 1 / 256) - 1 - s3Seg) * 256;
+                        if (s3Seg >= 255) {
+                            s2Seg = (int) (s3Seg + 1 / 256);
+                            s3Seg = ((s3Seg + 1 / 256) - 1 - s2Seg) * 256;
+                            if (s2Seg >= 255) {
                                 return -1;
                             }
                         }
@@ -73,9 +70,13 @@ public class SubnetCalculator {
                     hostSeg += 1;
                     broadcastAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
                     //Checking if the Host Segment is full and setting the s3Seg to the next one and resetting hostSeg
-                    if(hostSeg == 255) {
+                    if (hostSeg == 255) {
                         s3Seg += 1;
                         hostSeg = 0;
+                        if (s3Seg == 255) {
+                            s2Seg += 1;
+                            s3Seg = 0;
+                        }
                     } else {
                         hostSeg += 1;
                     }
@@ -87,20 +88,20 @@ public class SubnetCalculator {
                     ipRanges[i][3] = broadcastAddress;
                     //ipRanges[i][4] = subnetMask;
                 }
-                break;
-            case 'C':
-                //Setting the first two segments as they will never change in class C
-                s1Seg = 192;
-                s2Seg = 168;
+            }
+            case "B": {
+                //Setting the first twos segments as they will either never change or start at a non-0 number
+                s1Seg = 172;
+                s2Seg = 16;
                 //"all that jazz" -adam
-                for(int i:sHosts) {
+                for (int i : sHosts) {
                     //Returns error value of -1 (Subnet Size too large)
-                    if(i+2 > 256) {
-                        return(-1);
+                    if (i + 2 > 65536) {
+                        return (-1);
                     }
                     //Rounding up to the nearest square of two
                     int j = 0;
-                    while(i+2 <= nearest[j]) {
+                    while (i + 2 <= nearest[j]) {
                         j++;
                     }
                     //Converting the int segments into a whole dot separated address
@@ -108,23 +109,79 @@ public class SubnetCalculator {
                     hostSeg += 1;
                     firstHostAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
                     hostSeg += nearest[j] - 2;
+                    if (hostSeg >= 255) {
+                        s3Seg = (int) (hostSeg + 1 / 256);
+                        hostSeg = ((hostSeg + 1 / 256) - 1 - s3Seg) * 256;
+                        if (s3Seg >= 255) {
+                            s2Seg = (int) (s3Seg + 1 / 256);
+                            s3Seg = ((s3Seg + 1 / 256) - 1 - s2Seg) * 256;
+                            if (s2Seg >= 32) {
+                                return -1;
+                            }
+                        }
+                    }
                     lastHostAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
                     hostSeg += 1;
                     broadcastAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
                     //Checking if the Host Segment is full and setting the s3Seg to the next one and resetting hostSeg
-                    if(hostSeg == 255) {
+                    if (hostSeg == 255) {
                         s3Seg += 1;
                         hostSeg = 0;
+                        if (s3Seg == 255) {
+                            s2Seg += 1;
+                            s3Seg = 0;
+                        }
                     } else {
                         hostSeg += 1;
                     }
+                    //Setting subnet
+
                     ipRanges[i][0] = networkAddress;
                     ipRanges[i][1] = firstHostAddress;
                     ipRanges[i][2] = lastHostAddress;
                     ipRanges[i][3] = broadcastAddress;
                     //ipRanges[i][4] = subnetMask;
                 }
-                break;
+            }
+            case "C": {
+                //Setting the first two segments as they will never change in class C
+                s1Seg = 192;
+                s2Seg = 168;
+                //"all that jazz" -adam
+                int ii = 0;
+                for (int i : sHosts) {
+                    //Returns error value of -1 (Subnet Size too large)
+                    if (i + 2 > 256) {
+                        return (-1);
+                    }
+                    //Rounding up to the nearest square of two
+                    int j = 23;
+                    while (i + 2 <= nearest[j]) {
+                        j--;
+                    }
+                    //Converting the int segments into a whole dot separated address
+                    networkAddress = "" + s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
+                    hostSeg += 1;
+                    firstHostAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
+                    hostSeg += nearest[j] - 2;
+                    lastHostAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
+                    hostSeg += 1;
+                    broadcastAddress = s1Seg + "." + s2Seg + "." + s3Seg + "." + hostSeg;
+                    //Checking if the Host Segment is full and setting the s3Seg to the next one and resetting hostSeg
+                    if (hostSeg == 255) {
+                        s3Seg += 1;
+                        hostSeg = 0;
+                    } else {
+                        hostSeg += 1;
+                    }
+                    ipRanges[ii][0] = networkAddress;
+                    ipRanges[ii][1] = firstHostAddress;
+                    ipRanges[ii][2] = lastHostAddress;
+                    ipRanges[ii][3] = broadcastAddress;
+                    //ipRanges[ii][4] = subnetMask;
+                    ii++;
+                }
+            }
         }
         return 0;
     }
@@ -132,11 +189,11 @@ public class SubnetCalculator {
     public void setClassful(boolean classful) {
         this.classful = classful;
     }
-    public char getSubnetClass(){
+    public String getSubnetClass(){
         return sClass;
     }
 
-    public void setSubnetClass(char sClass) {
+    public void setSubnetClass(String sClass) {
         this.sClass = sClass;
     }
 
